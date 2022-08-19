796f1c04d716170030116e15a9984a6af8094b08
Jonas Teetaert <jteetaert95@gmail.com>
2022-08-19 16:12:46 +0200

    created coming soon page

---
+++ src/pages/maintenance.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const Maintenance = () => {
+  return (
+    <Layout fullpage={true}>
+      <title>Maintenance</title>
+      <h1>Coming Soon</h1>
+    </Layout>
+  )
+}
+
+export default Maintenance
