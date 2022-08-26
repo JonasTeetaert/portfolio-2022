6d7bc3b486245ad171bdfabc09d1ff4ff4929e6c
Jonas Teetaert <jteetaert95@gmail.com>
2022-08-26 16:06:29 +0200

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,10 +7,31 @@ module.exports = {
     title: 'Jonas Teetaert - Creative Frontend Developer | Portfolio',
     description:
       'Jonas Teetaert is a Brussels based Frontend Developer passionated about how web animations can enhance a better user experience. He makes websites and web applications from scratch using technologies as Craft CMS and React, fully customised to the customer’s needs.',
-    image: '/static/icons/favicon/mstile-150x150.png',
+    image: '/icons/favicon/mstile-150x150.png',
     siteUrl: 'https://www.jonasteetaert.be/',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-48C1RSDJ2X', // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: 'OPT_CONTAINER_ID',
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
     'gatsby-plugin-image',
